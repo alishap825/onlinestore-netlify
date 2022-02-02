2e7d72d67877c90a161b60494d5a166be9f4769b
alishap825 <alischett123@gmail.com>
2022-02-02 13:35:12 +0545

    commitchanges

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [product, setProduct] = useState<any[]>([]);
+  console.log(product);
+  useEffect(() => {
+    fakeStore();
+  }, [])
+
+  const fakeStore = async () => {
+    const response = await fetch('https://fakestoreapi.com/products');
+    // console.log (response);
+    const jsonData= await response.json();
+    // console.log(jsonData);
+    setProduct(jsonData);
+  }
+  // fakeStore();
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ONLINE STORE</h1>
+      <div className="container">
+      {product.map((values)=>{
+        return(
+          <>
+          <div className="box">
+          <div className="content">
+            <h3>{values.title}</h3>
+            <h4>${values.price}</h4>
+            <p>{values.description}</p>
+          </div>
+           <img src={values.image} alt="" />
+        </div>
+          </>
+        )
+      })}
+      </div>
     </div>
-  );
+  
+)
 }
-
 export default App;
+    
