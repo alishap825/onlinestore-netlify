a6ff36352c203dd0071b2ff23d889e5c3fc5f0ae
alishap825 <alischett123@gmail.com>
2022-02-02 17:14:07 +0545

    loadingindicator

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React,{useState, useEffect} from 'react';
+import { BounceLoader } from 'react-spinners';
+import { jsx, css } from '@emotion/react';
 import './App.css';
 
 
 
 function App() {
   const [product, setProduct] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
   console.log(product);
   useEffect(() => {
+    const fakeStore = async () => {
+      setIsLoading(true);
+      setHasError(false);
+      try {
+      const response = await fetch('https://fakestoreapi.com/products');
+      // console.log (response);
+      const jsonData= await response.json();
+      // console.log(jsonData);
+      setProduct(jsonData);
+    } catch (error) {
+      setHasError(true);
+      }
+      setIsLoading(false);
+    };
     fakeStore();
-  }, [])
+  }, [setProduct]);
 
-  const fakeStore = async () => {
-    const response = await fetch('https://fakestoreapi.com/products');
-    // console.log (response);
-    const jsonData= await response.json();
-    // console.log(jsonData);
-    setProduct(jsonData);
-  }
+ const loaderCSS = css`
+ margin-top: 300px;
+ margin-bottom: 400px;
+`
   // fakeStore();
   
   return (
     <div className="App">
+    <React.Fragment>
+    {hasError && <p>Something went wrong.</p>}
+    {isLoading ? (
+      <BounceLoader css={loaderCSS} size={100} color='#082044' loading />
+      ) : (
+        <ul>
       <h1>ONLINE STORE</h1>
       <div className="container">
       {product.map((values)=>{
@@ -38,9 +59,12 @@ function App() {
         )
       })}
       </div>
+      </ul>
+       )}
+    </React.Fragment>
     </div>
   
-)
+);
 }
 export default App;
     
